e9391c39a44157ba7bff6edc7345898c745c7d7e
ryoji3333 <superfree1111@yahoo.co.jp>
2020-06-17 20:34:05 +0900

    tasklist-edit

--- app/assets/javascripts/modal-window.js
+++ app/assets/javascripts/modal-window.js
@@ -1,12 +1,13 @@
 $(function(){
   $('.js-modal-open').on('click',function(){
-      let tasklist = $(this).data("id")
-      $( '.js-modal[data-id = "' + tasklist + '"]' ).show();
+      let id = $(this).data("id")
+      $( '.js-modal[data-id = "' + id + '"]' ).show();
       $('input:visible').eq(0).focus();
       return false;
   });
   $('.js-modal-close').on('click',function(){
       $('.js-modal').hide();
+      // $('.send-task-form').submit();
       return false;
   });
 });
@@ -19,6 +20,7 @@ $(function(){
   });
   $('.js-modal-close-tasklist').on('click',function(){
       $('.js-modal-tasklist').hide();
+      $('.send-tasklist-form').submit();
       return false;
   });
 });
@@ -27,11 +29,30 @@ $(function(){
   $('.js-modal-open-edittask').on('click',function(){
       let id = $(this).data("id")
       $( '.js-modal-edittask[data-id = "' + id + '"]' ).show();
-      $('input:visible').eq(0).focus();
+      var v = $('input:visible').eq(0).val();
+      $('input:visible').eq(0).val('');
+      $('input:visible').eq(0).focus().val(v);
       return false;
   });
   $('.js-modal-close-edittask').on('click',function(){
-      $('.js-modal-edittask').hide();
+    $('.js-modal-edittask').hide();
+    $('.send-edittask-form').submit();
+    return false;
+  });
+});
+
+$(function(){
+  $('.js-modal-open-edittasklist').on('click',function(){
+      let id = $(this).data("id")
+      $( '.js-modal-edittasklist[data-id = "' + id + '"]' ).show();
+      var v = $('input:visible').eq(0).val();
+      $('input:visible').eq(0).val('');
+      $('input:visible').eq(0).focus().val(v);
+      return false;
+  });
+  $('.js-modal-close-edittasklist').on('click',function(){
+      $('.js-modal-edittasklist').hide();
+      $('.send-edittasklist-form').submit();
       return false;
   });
 });
